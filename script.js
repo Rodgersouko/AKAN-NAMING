df6bcde6126d98af83209e5c0114bed244b76eb3
Rodgersouko <gabrielaouko@gmail.com>
2020-08-22 08:38:22 +0300

    my files

---
+++ script.js
@@ -0,0 +1,12 @@
+var submission= function(){
+    var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleNames = ["Akosua"," Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];   
+    return days ,maleNames ,femaleNames
+};
+var name=document.getElementById("name");
+var year=document.getElementById("year");
+var month=document.getElementById("month");
+var day=document.getElementById("day");
+var male=document.getElementById("male");
+var female=document.getElementById("female");
