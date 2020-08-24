a1d193e26740ae43666267c0cd3b35b195867352
Rodgersouko <gabrielaouko@gmail.com>
2020-08-24 07:00:28 +0300

    spanned js var

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
 var submission = function() {
-    var days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    var maleNames = [
-      "Kwasi",
-      "Kwadwo",
-      "Kwabena",
-      "Kwaku",
-      "Yaw",
-      "Kofi",
-      "Kwame"
-    ];
-    var femaleNames = [
-      "Akosua",
-      "Adwoa",
-      "Abenaa",
-      "Akua",
-      "Yaa",
-      "Afua",
-      "Ama"
-    ];
-   
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; 
+    var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     var name =document.getElementById("name").value;
     var year = parseInt(document.getElementById("year").value);
     var month = parseInt(document.getElementById("month").value);
